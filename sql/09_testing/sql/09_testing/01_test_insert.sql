8ec6bcc1356cdffadb2305c46f129529d6406750
Kiran-data-lab <tentu.kiran@gmail.com>
2026-09-14 22:18:32 +0530

    Insert data and add verification query
    Added a verification query to check inserted data.

---
+++ sql/09_testing/sql/09_testing/01_test_insert.sql
@@ -0,0 +1,11 @@
+-- ============================================================
+-- CDC INSERT Test
+-- ============================================================
+
+INSERT INTO PRACTICE_DB.TRAINING.STREAM_DEMO_EMP
+VALUES
+(121, 'Naveen', 'Chennai', 485000);
+
+--Verification query
+SELECT *
+FROM PRACTICE_DB.TRAINING.STREAM_DEMO_EMP_CDC;
